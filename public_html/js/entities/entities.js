906cba1bd5c566029dcea094cf4b671a08443f5d
kristopher jones <grofernioken@gmail.com>
2014-12-02 14:03:20 -0800

    did it work

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -32,7 +32,22 @@ game.PlayerEntity = me.Entity.extend({
             this.renderable.setCurrentAnimation("idle");
         }
         this.body.update(delta);
+        me.collision.check(this, true, this.collideHandler.bind(this), true);
         this._super(me.Entity, "update", [delta]);
         return true;
-   }
+   },
+   
+   
+});
+
+game.LevelTrigger = me.Entity.extend({
+    init: function(x, y, settings){
+        this._super(me.Entity, 'init', [x, y, settings]);
+        this.body.onCollision = this.onCollision.bind(this);
+        this.level = settings.level;
+    },
+    onCollision: function(){
+        this.body.netCollisionMask(me.collision.types.NO_OBJECT);
+        me.levelDirector.loadLevel(this.level);
+    }
 });
--- public_html/js/game.js
+++ public_html/js/game.js
@@ -39,6 +39,7 @@ var game = {
 	// Run on game resources loaded.
 	"loaded" : function () {
                 me.pool.register("mario", game.PlayerEntity, true);
+                me.pool.register("LevelTrigger", LevelTrigger)
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
 
